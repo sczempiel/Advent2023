3fe60904cf5b5a492097e5fb9bfec46ba70f344c
Sebastian Schubert <s.czempiel@web.de>
2022-12-04 06:47:34 +0100

    day4

---
+++ src/day04/Day4Task1Main.java
@@ -0,0 +1,37 @@
+package day04;
+
+import java.io.IOException;
+import java.util.List;
+
+import util.AdventUtils;
+
+public class Day4Task1Main {
+
+	public static void main(String[] args) {
+		try {
+			List<String> pairs = AdventUtils.getStringInput(4);
+
+			int count = 0;
+
+			for (String pair : pairs) {
+
+				String[] assignments = pair.split("[,-]");
+
+				int lower1 = Integer.parseInt(assignments[0]);
+				int upper1 = Integer.parseInt(assignments[1]);
+
+				int lower2 = Integer.parseInt(assignments[2]);
+				int upper2 = Integer.parseInt(assignments[3]);
+
+				if ((lower1 >= lower2 && upper1 <= upper2) || (lower2 >= lower1 && upper2 <= upper1)) {
+					count++;
+				}
+
+			}
+
+			AdventUtils.publishResult(4, 1, count);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
